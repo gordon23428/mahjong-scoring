8b17c876a6b6bbe3c95b35ca81a0108f4323c8ae
gordon <pigsong23428@gmail.com>
2024-11-29 17:22:55 +0800

    引入element plus

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<el-button plain @click="dialogVisible = true"> 開始 </el-button>
+	<el-dialog v-model="dialogVisible" title="Tips" width="500" :before-close="handleClose">
+		<span>This is a message</span>
+		<template #footer>
+			<div class="dialog-footer">
+				<el-button @click="dialogVisible = false">Cancel</el-button>
+				<el-button type="primary" @click="dialogVisible = false"> Confirm </el-button>
+			</div>
+		</template>
+	</el-dialog>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
