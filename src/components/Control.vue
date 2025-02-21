3957f1d67999458c7844fd06568f1aeee33b47e2
gordon <pigsong23428@gmail.com>
2025-02-21 18:18:11 +0800

    新增Control元件

---
+++ src/components/Control.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-button plain @click="start"> {{ text }} </el-button>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useInfoStore } from '@/store/info.js'
+import { storeToRefs } from 'pinia'
+
+const infoStore = useInfoStore()
+const { players } = storeToRefs(infoStore)
+const isStarted = computed(() => players.value.length > 0)
+const text = computed(() => isStarted.value ? '重新開始' : '開始')
+
+const emit = defineEmits(['start'])
+const start = () => {
+  emit('start')
+}
+
+</script>
